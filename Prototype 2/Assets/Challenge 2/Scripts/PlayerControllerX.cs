00b2c685a42c93ca6f5f15a1b147bfbedfcf77f7
Miriaana <7h.miriam@gmail.com>
2023-04-13 12:49:35 +0200

    prototype 2 finished

---
+++ Prototype 2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float fullCooldown = 0.5f;
+    private float currentCooldown = 0.0f;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentCooldown += Time.deltaTime;
+
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && currentCooldown >= fullCooldown)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            currentCooldown = 0.0f;
+        }
+    }
+}
---
+++ Prototype 2/Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManagerX : MonoBehaviour
+{
+    public GameObject[] ballPrefabs;
+
+    private float spawnLimitXLeft = -22;
+    private float spawnLimitXRight = 7;
+    private float spawnPosY = 30;
+
+    private float startDelay = 1.0f;
+    private float spawnIntervalMin = 3.0f;
+    private float spawnIntervalMax = 5.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("SpawnRandomBall", startDelay);
+    }
+
+    // Spawn random ball at random x position at top of play area
+    void SpawnRandomBall ()
+    {
+
+        // Generate random ball index and random spawn position
+        int ballIndex = Random.Range(0, ballPrefabs.Length);
+        Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
+
+        // instantiate ball at random spawn location
+        Instantiate(ballPrefabs[ballIndex], spawnPos, ballPrefabs[ballIndex].transform.rotation);
+
+        //spawnInterval is called each time "SpawnRandomBall" is called
+        float spawnInterval = Random.Range(spawnIntervalMin, spawnIntervalMax);
+
+        Debug.Log(spawnInterval);
+
+        //Recursively call the method after the spawnInterval has passed
+        Invoke("SpawnRandomBall", spawnInterval); 
+    }
+
+}
